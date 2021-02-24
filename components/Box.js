04ca96c65aa1ba97b4271e1a52696a7de5e799ae
thomasdevolder <thomas.devolder@student.howest.be>
2021-02-24 09:55:07 +0100

    my changes

---
+++ components/Box.js
@@ -0,0 +1,9 @@
+import {render, Box} from 'ink';
+
+const Example = () => (
+	<Box margin={2}>
+		<Text>This is a box with margin</Text>
+	</Box>
+);
+
+render (<Example/>);
--- ui.js
+++ ui.js
@@ -1,13 +1,15 @@
 'use strict';
 const React = require('react');
 const { Text } = require('ink');
+const Example = importJSX('./components/Box');
 
 const App = ({ name = 'Stranger' }) => (
 	<>
 		<Text>
 			Hello, <Text color="green">{name}</Text>
-			I am a retard
+			 I am a retard
 		</Text>
+		<Example/>
 	</>
 );
 
