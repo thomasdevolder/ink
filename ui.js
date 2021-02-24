f2a1129052e205c6cf92c7f6964b2cb52b85663c
Alexander Buysse <alexanderbuysse002@gmail.com>
2021-02-24 10:57:32 +0100

    snake is nu een componennt

--- ui.js
+++ ui.js
@@ -1,12 +1,10 @@
 'use strict';
 const React = require('react');
 const { Text } = require('ink');
-const snake = require('snake-cli');
 
 // Deze lijn zorgt ervoor dat je externe jsx kan importeren
 const importJsx = require('import-jsx');
-const TestComp = importJsx("./components/TestComp.js");
-const Box = importJsx("./components/Box.js")
+const SnakeComp = importJsx("./components/SnakeComp.js");
 
 const name = "Dit is een testnaam";
 
@@ -21,8 +19,7 @@ const App = ({ }) => (
 	<Text>
 		Hello, my name is <Text color="green">{name}</Text>
 		<Component />
-		{<TestComp />}
-		{snake()}
+		{<SnakeComp />}
 	</Text>
 );
 
