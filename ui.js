135475b9288074e34091cfdc0ec6d48a698afd5c
Eerke2001 <erin.troch@student.howest.be>
2021-02-24 09:38:47 +0100

    test

--- ui.js
+++ ui.js
@@ -1,14 +1,13 @@
 'use strict';
 const React = require('react');
-const {Text} = require('ink');
+const { Text } = require('ink');
 
-const App = ({name = 'Stranger'}) => (
-	<Text>
-		Hello, <Text color="green">{name}</Text>
-	</Text>
-	<Text>
-		HEllO I am a little bit retarded
-	</Text>
+const App = ({ name = 'Stranger' }) => (
+	<>
+		<Text>
+			Hello, <Text color="green">{name}</Text>
+		</Text>
+	</>
 );
 
 module.exports = App;
